c540bfed6b2762c1734f4992beda626c5da1fad1
kunstler <georges.kunstler@gmail.com>
2016-07-31 01:41:48 +0200

    vectorised but slower

--- R/RTheoModelFun.R
+++ R/RTheoModelFun.R
@@ -87,45 +87,19 @@ return(mat.index.neigh.cells)
 }
 
 
-function.array.neigcells <-  function(NN,Nlandscape){
-array.i <- matrix(NA,dim=c(NN,NN*Nlandscape,8))
-array.j <- matrix(NA,dim=c(NN,NN*Nlandscape,8))
-
-for (i in 1:NN)
-{
- for (j in 1:(NN*Nlandscape))
-   {
-
-n.i <-  function.torus(i,N,imax=NN)
-n.j <- function.reflex(i=j,N,imax=NN*Nlandscape)
-array.i[i,j,]<-  c(rep(c(n.i[1:N],i,n.i[(N+1):2*N]),N),n.i,
-                   rep(c(n.i[1:N],i,n.i[(N+1):2*N]),N))
-array.j[i,j,]<- c(rep(n.j[1:N],each=2*N+1),rep(j,each=2*N),
-                  rep(n.j[(N+1):(2*N)],each=2*N+1))
-
-   }
-}
-return(list(array.i,array.j))
-}
-
-
-### function to disperse all seed from neigborhood into a given cell
-## disp.fun function with both the dispersal model and the fecundity
-## then draw in a poisson distribution form average seed
-
-## function to get the matrix value
-function.return.cell <- function(n,i,j,mat) return(mat[i[n],j[n]])
-
 
 ### PROBABLY NOT THE MOST EFFEICIENT TO IMPROVE /THERE WAS NEED TO INCLUDE THE INDIVIDUAL PRESENT IN THE CELL Succ
-function.disperse.Succ.to.ij <- function(i,j,Alandscape.LIST,array.i,array.j){
-ccc.E.vec <-Alandscape.LIST[[1]][cbind(array.i[i,j,], array.j[i,j,])]
-ccc.L.vec <-Alandscape.LIST[[2]][cbind(array.i[i,j,], array.j[i,j,])]
-return(list(E = as.vector(na.exclude(c(Alandscape.LIST[[1]][i,j],ccc.E.vec))),
-            L = as.vector(na.exclude(c(Alandscape.LIST[[2]][i,j],ccc.L.vec)))))
+return.val.neigh <- function(select, val, mat.neigh){
+matrix(val[as.vector(mat.neigh[select, ])], sum(select) , 8)
 }
 
-
+sample.mat <-  function(x,y){
+  res <-  NA
+  select <- !is.na(x)
+  res <- tryCatch(sample(x[select], size = 1, prob = y[select]),
+             error = function(e) NA)
+return(res)
+}
 
 #########
 #########
@@ -133,202 +107,166 @@ return(list(E = as.vector(na.exclude(c(Alandscape.LIST[[1]][i,j],ccc.E.vec))),
 
 ## based on continous law function
 function.law <- function(x,y,K){
-return(1/(1+exp(-K*(x-y))))
+return(1/(1 + exp(-K*(x - y))))
 }
 
 
 ### compute proba of winning all pairs  competitive events
 function.compet <-  function(vec.t,K){
-mt <- outer(vec.t,vec.t,FUN=function.law,K=K)
-diag(mt) <-  1
-return(apply(mt,MARGIN=1,FUN=prod)/sum(apply(mt,MARGIN=1,FUN=prod)))
+ require(matrixStats)
+  mt <- outer(vec.t, vec.t, FUN = function.law, K = K)
+  diag(mt) <-  1
+  pp <- rowProds(mt, na.rm = TRUE)
+  pp[is.na(vec.t)] <-  NA
+  return(pp/sum(pp, na.rm = TRUE))
 }
 
-colonize.E <- function(vec.c.seed, param.K, Alandscape.LIST){
-vec.res <- c(NA,NA)
-      if(length(vec.c.seed[[1]])>0) {
-          winner <- sample(1:length(vec.c.seed[[1]]),
-                           size = 1,
-                           prob = function.compet(vec.t=vec.c.seed[[1]],
-                                                  K=param.K))
-          vec.res[1] <- vec.c.seed[[1]][winner]
-          vec.res[2] <- vec.c.seed[[2]][winner]
-      }
-      else {
-         vec.res[1] <-(Alandscape.LIST[[1]][i,j])
-         vec.res[2] <-(Alandscape.LIST[[2]][i,j])
-      }
-return(vec.res)
+colonize.E <- function(mat.state, Ncells, mat.neigh, df.sp, param.K){
+  select <- mat.state[, "Succ"] ==2
+  mat.neigh.val <- cbind(mat.state[select, "E"],
+                         return.val.neigh(select, mat.state[, "E"], mat.neigh))
+  mat.prob <- t(apply(mat.neigh.val, MARGIN = 1, function.compet, K = param.K))
+  new.val <- mapply(sample.mat, split(mat.neigh.val, row(mat.neigh.val)),
+                                     split(mat.prob, row(mat.prob)))
+  mat.state[select, "E"] <- new.val
+  mat.state[select, "L"] <- df.sp[match(new.val, df.sp$E), "L"]
+  return(mat.state[ , 1:2])
 }
 
-colonize.L <- function(vec.c.seed, param.K, Alandscape.LIST){
-vec.res <- c(NA,NA)
-     if(length(vec.c.seed[[2]])>0) {
-         winner <- sample(1:length(vec.c.seed[[2]]),
-                          size = 1,
-                          prob = function.compet(vec.t = vec.c.seed[[2]],
-                                                 K = param.K))
-                      vec.res[1] <- vec.c.seed[[1]][winner]
-                      vec.res[2] <- vec.c.seed[[2]][winner]
-     } else {
-         vec.res[1] <-(Alandscape.LIST[[1]][i,j])
-         vec.res[2] <-(Alandscape.LIST[[2]][i,j])
-     }
-return(vec.res)
-}
-
-colonize.NO <- function(vec.c.seed, param.K, Alandscape.LIST){
-vec.res <- c(NA,NA)
-     if(length(vec.c.seed[[1]])>0) {
-         winner <- sample(1:length(vec.c.seed[[1]]),
-                          size = 1,
-                          prob = function.compet(vec.t=vec.c.seed[[1]],
-                                                 K=param.K))
-         vec.res[1] <- vec.c.seed[[1]][winner]
-         vec.res[2] <- vec.c.seed[[2]][winner]
-     } else {
-         vec.res[1] <-NA
-         vec.res[2] <-NA
-      }
-return(vec.res)
-}
-
-
-############################################################
-## function to update a given cell with new colonizer
-### THIS NEZ FUNCTION IS CHANGED TO HAVE TO STATE OF THE CELL EITHER EARLY SUCC OR LATE SUCC
-function.colonize.cell.Succ <-  function(i,j,param.K,
-                                         Alandscape.LIST,array.i,array.j){
-vec.c.seed <- function.disperse.Succ.to.ij(i, j,
-                                           Alandscape.LIST,
-                                           array.i, array.j)
-vec.res <- switch(Alandscape.Succ[i,j],
-                  E = colonize.E(vec.c.seed, param.K, Alandscape.LIST),
-                  L = colonize.L(vec.c.seed, param.K, Alandscape.LIST),
-                  NO = colonize.NO(vec.c.seed, param.K, Alandscape.LIST))
-return(vec.res)
-}
-
-
-
-#######################################################################
-### function to kill a cell with disturbance and climate stress
-function.kill <-  function(i,j,Alandscape,param.climate.stress,param.dist,fun.clim.morta){
-## global dist
-
- if(is.na(Alandscape[i,j])){
-    return(NA)
- }else{
-  if(runif(1)<param.dist) {
-      return(NA)
-  }else{
-  ## climate death
-  if(runif(1)>fun.clim.morta(Alandscape[i,j],j,param.climate.stress)){
-      return(NA)
-   }else{
-      return(Alandscape[i,j])
+colonize.L <- function(mat.state, Ncells, mat.neigh, df.sp, param.K){
+  if(sum(mat.state[, "Succ"] ==3) > 0){
+     select <- mat.state[, "Succ"] ==3
+     mat.neigh.val <- cbind(mat.state[select, "L"],
+                            return.val.neigh(select, mat.state[, "L"], mat.neigh))
+     mat.prob <- t(apply(mat.neigh.val, MARGIN = 1, function.compet, K = param.K))
+     new.val <- mapply(sample.mat, split(mat.neigh.val, row(mat.neigh.val)),
+                                        split(mat.prob, row(mat.prob)))
+     mat.state[select, "L"] <- new.val
+     mat.state[select, "E"] <- df.sp[match(new.val, df.sp$L), "E"]
    }
-  }
- }
+  return(mat.state[ , 1:2])
+}
+
+colonize.NO <- function(mat.state, Ncells, mat.neigh, df.sp, param.K){
+
+   select <- mat.state[, "Succ"] ==1
+   mat.sel <- mat.state[select, ]
+   mat.neigh.val <- return.val.neigh(select, mat.state[, "E"], mat.neigh)
+   mat.prob <- t(apply(mat.neigh.val, MARGIN = 1, function.compet, K = param.K))
+   new.val <- mapply(sample.mat, split(mat.neigh.val, row(mat.neigh.val)),
+                                      split(mat.prob, row(mat.prob)))
+   mat.sel[ , "E"] <- new.val
+   mat.sel[!is.na(new.val), "L"] <- df.sp[match( new.val[!is.na(new.val)],
+                                                df.sp$E), "L"]
+   mat.sel[mat.sel[ , "Succ"] ==1 & !is.na(mat.sel[, "E"]) , "Succ"] <- 2
+   mat.state[select, ] <- mat.sel
+   return(mat.state[, 1:3])
 }
 
 
+kill.cells <- function(mat.state, Ncells,
+                       param.dist){
 
-## climate mortality function
-fun.clim.morta1 <- function(c,j,param.climate.stress){
-1-c*climate.grad[j]
+  select <- (1:Ncells)[mat.state[, "Succ"] !=1]
+  mat.sel <- mat.state[select, ]
+  prob.dead.stress <- apply(mat.sel, MARGIN = 1, sum)
+  dead.dist <- runif(length(select))<param.dist
+  dead.stress <- runif(length(select)) > prob.dead.stress
+  dead.dist | dead.stress
+  mat.sel[dead.dist | dead.stress, 1:2] <-  NA
+  mat.sel[dead.dist | dead.stress, 3] <-  1
+  mat.state[select, ] <- mat.sel
+  return(mat.state)
 }
 
-## function.kill(i=1,j=20,Alandscape=AlandscapeE+AlandscapeL,param.climate.stress=NA,param.dist=0.0,fun.clim.morta=fun.clim.morta1)
 
-
-####
-## FUNCTION FOR SUCCESSION FROM E TO L
-fun.Succ <-  function(i,j,Alandscape.LIST,param.Succ){
-Succ.temp <- Alandscape.LIST[[3]][i,j]
-if(Alandscape.LIST[[3]][i,j]=="E"){
-    if(runif(1)<param.Succ){Succ.temp <- "L"}
-  }
-if(Alandscape.LIST[[3]][i,j]=="NO" & !is.na(Alandscape.LIST[[1]][i,j])){
-    if(runif(1)<param.Succ){Succ.temp <- "E"}
-  }
-
-return(Succ.temp)
+succ.cells <- function(mat.state, Ncells,
+                       param.succ){
+if(sum(mat.state[, "Succ"] ==2)>0){
+  select <- (1:Ncells)[mat.state[, "Succ"] ==2]
+  succ.L <- runif(length(select))<param.succ
+  mat.state[select, "Succ"][succ.L] <- 3
+}
+  return(mat.state[, 'Succ'])
 }
 
-############################
-############################
-###### FUNCTION TO UPDATE THE LANDSCAPE ONE STEP
-
-
-####
-fun.update.landscape.Succ<- function(Alandscape.LIST,fun.clim.morta,
-                                     param.K,param.climate.stress,
-                                     param.dist,param.Succ,array.i,array.j){
-for (i in 1:nrow(Alandscape.LIST[[1]])){
-   for (j in 1:ncol(Alandscape.LIST[[1]])){
-       vec.EL <- function.colonize.cell.Succ(i,j,param.K,
-                                             Alandscape.LIST=Alandscape.LIST,
-                                             array.i,array.j)
-       Alandscape.LIST[[1]][i,j] <- vec.EL[1]
-       Alandscape.LIST[[2]][i,j] <- vec.EL[2]
-
-       Alandscape.LIST[[3]][i,j] <- fun.Succ(i,j,Alandscape.LIST,param.Succ)
-
-       morta <- function.kill(i,j,
-                              Alandscape= Alandscape.LIST[[1]]+
-                                          Alandscape.LIST[[2]],
-                              param.climate.stress,
-                              param.dist,
-                              fun.clim.morta)
-       if(is.na(morta)){
-        Alandscape.LIST[[1]][i,j] <- NA
-        Alandscape.LIST[[2]][i,j] <- NA
-        Alandscape.LIST[[3]][i,j] <- "NO"
-        }
-     }
-  }
-return(Alandscape.LIST)
+update.cells <- function(mat.state, mat.neigh, Ncells, df.sp,  param.K, param.dist, param.succ){
+  mat.state[ , 1:2] <- colonize.E(mat.state, Ncells, mat.neigh, df.sp, param.K)
+  mat.state[ , 1:2] <- colonize.L(mat.state, Ncells, mat.neigh, df.sp, param.K)
+  mat.state[ , 1:3] <- colonize.NO(mat.state, Ncells, mat.neigh, df.sp, param.K)
+  mat.state[ , "Succ"] <- succ.cells(mat.state, Ncells, param.succ)
+  mat.state <- kill.cells(mat.state, Ncells, param.dist)
+  return(mat.state)
 }
-
 
 #########
 ## function run simulation
-fun.run.sim.Succ <- function(A,B,Alandscape.LIST.init,
-                             fun.clim.morta=fun.clim.morta1,
-                             param.K=1,param.climate.stress=NA,param.dist=0.1,
-                             param.Succ,array.i,array.j){
-
-res.listE.temp <- list()
-res.listL.temp <- list()
-res.list.Succ.temp <- list()
-res.listE.temp[[1]] <- Alandscape.LIST.init[[1]]
-res.listL.temp[[1]] <- Alandscape.LIST.init[[2]]
-res.list.Succ.temp[[1]] <- Alandscape.LIST.init[[3]]
-Alandscape.LIST <-  Alandscape.LIST.init
-
+fun.run.sim.succ <- function(A,B, mat.state, mat.neigh, Ncells, df.sp, param.K, param.dist, param.succ){
+res.list.temp <- list()
+res.list.temp[[1]] <- mat.state
 for(j in 1:A){
   for (i in 1:B){
-Alandscape.LIST <-  fun.update.landscape.Succ(Alandscape.LIST,fun.clim.morta=fun.clim.morta,
-                                              param.K, param.climate.stress,
-                                              param.dist,param.Succ,array.i,array.j)
+mat.state <- update.cells(mat.state, mat.neigh, Ncells, df.sp, param.K, param.dist, param.succ)
   }
-res.listE.temp[[j+1]] <- Alandscape.LIST[[1]]
-res.listL.temp[[j+1]] <- Alandscape.LIST[[2]]
-res.list.Succ.temp[[j+1]] <- Alandscape.LIST[[3]]
-gc()
+res.list.temp[[j+1]] <- mat.state
 }
-return(list(res.listE.temp,res.listL.temp,res.list.Succ.temp))
+return(res.list.temp)
 }
 
+
+generate.rand.sp <- function(nsp, Nval = 10000){
+   if(nsp > 5000) stop("error nsp must be smaller than 5000")
+   E <- matrix(rep(0:Nval, Nval + 1), Nval +1, Nval+1)
+   L <- matrix(rep(0:Nval, Nval + 1), Nval + 1, Nval +1, byrow= TRUE)
+   S <- (E+L)
+   E.r <-  E[rev(0:(Nval + 1)), ]
+   L.r <-  L[rev(0:(Nval + 1)), ]
+   S.r <-  S[rev(0:(Nval + 1)), ]
+   E.r[upper.tri(E.r,diag = FALSE)] <-  NA
+   L.r[upper.tri(E.r,diag = FALSE)] <-  NA
+   S.r[upper.tri(E,diag = FALSE)] <-  NA
+   E.b <- E.r[rev(0:(Nval + 1)), ]
+   L.b <- L.r[rev(0:(Nval + 1)), ]
+   S.b <- S.r[rev(0:(Nval + 1)), ]
+
+   df <- na.omit(data.frame(E = as.vector(E.b)/Nval,
+                            L = as.vector(L.b)/Nval,
+                            S = as.vector(S.b)/Nval))
+  df.sp <- df[sample(1:dim(df)[1], nsp+4), ]
+  df.sp <- df.sp[!duplicated(df.sp$E) & !duplicated(df.sp$L), ]
+   return(df.sp)
+}
+
+
+plot.sp <- function(df){
+   par(mfrow=c(2,2))
+   x   <- cbind(df$E , df$L)
+   data <- as.data.frame(x)
+   names(data) <- c("X","Y")
+   colors  <- densCols(x)
+   plot(x, col=colors, pch=20,cex=0.25,xlab="C.E",ylab="C.L")
+   abline(a = 1, b = -1, col = 'red')
+   x   <- cbind(1 - df$E - df$L, df$L)
+   data <- as.data.frame(x)
+   names(data) <- c("X","Y")
+   colors  <- densCols(x)
+   plot(x, col=colors, pch=20,cex=0.25,xlab="S",ylab="C.L")
+   abline(a = 1, b = -1, col = 'red')
+   x   <- cbind(df$E, 1 - df$E - df$L)
+   data <- as.data.frame(x)
+   names(data) <- c("X","Y")
+   colors  <- densCols(x)
+   plot(x, col=colors, pch=20,cex=0.25,xlab="C.E",ylab="S")
+   abline(a = 1, b = -1, col = 'red')
+}
 
 ###################################################
 ###################################################
 ### FUNCTION TO ANALYSE OUTPUTS
 
-function.table.levels <-  function(v){
-   vec_sp <- factor(v, levels = (0:100)/100)
-    table(vec_sp)/sum(table(vec_sp))
+function.table.levels <-  function(v, nn = 1){
+   vec_sp <- table(cut(v[,nn], breaks = (0:100)/100))
+   vec_sp/sum(vec_sp, na.rm = TRUE)
 }
 
 
--- cluster.simul.Succ.theotest.R
+++ cluster.simul.Succ.theotest.R
@@ -14,134 +14,72 @@ source(file="R/RTheoModelFun.R")
 
 
 ##################################
-## initialization  Succ
+## initialization  Succ TODO wrap up in a function
 ## Landscapoe matrix
-Nlandscape <-  1 #dimension (in 2*NN) of the long side corressponding to the climatic stress gradient
+Nlandscape <-  1 #dimension (in Nlandscape*NN) of the long side corressponding to the climatic stress gradient
 NN <- 25  ## size of landscape (classicaly 256)
-N <-  1 ## number of neigborhood cell 1 = moor neigborhood
-AlandscapeE <-  matrix(NA,nrow=NN,ncol=Nlandscape*NN)
-AlandscapeL <- AlandscapeE
-Alandscape.Succ <- matrix("NO",nrow=NN,ncol=Nlandscape*NN)
-
-## climate gradient
+matrix.cells.index <- matrix(1:(NN*NN*Nlandscape), nrow=NN,ncol=Nlandscape*NN)
 climate.grad <-  seq(from=0,to=0,length=Nlandscape*NN)
 
-## init landscape with random draw of competition traits
+matrix.cells.stress <- matrix(rep(climate.grad , NN),
+                              nrow=NN, ncol=Nlandscape*NN,
+                              byrow = TRUE)
+
+mat.state.init <- matrix(NA, nrow = (NN*NN*Nlandscape), ncol = 4)
+colnames(mat.state.init) <- c("E", "L", "Succ", "Stress")
+mat.state.init[, "Succ"] <-  1
+mat.state.init[, "Stress"] <-  as.vector(matrix.cells.stress)
+
+
+
+## init landscape with species
 init.temp <- sample(1:(NN*NN*Nlandscape),size=round(NN*NN*Nlandscape*0.5))
-vec.land <- as.vector(AlandscapeE)
-vec.landE <- as.vector(AlandscapeE)
-vec.landL <- as.vector(AlandscapeE)
-vec.land.Succ <- as.vector(Alandscape.Succ)
 
 ### create random species with a tradeoff between competition stress tolerance and eraly and late succ compet
-E <- matrix(rep(0:100, 101), 101, 101)
-L <- matrix(rep(0:100, 101), 101, 101, byrow= TRUE)
-S <- (E+L)
-E.r <-  E[rev(0:101), ]
-L.r <-  L[rev(0:101), ]
-S.r <-  S[rev(0:101), ]
-E.r[upper.tri(E.r,diag = FALSE)] <-  NA
-L.r[upper.tri(E.r,diag = FALSE)] <-  NA
-S.r[upper.tri(E,diag = FALSE)] <-  NA
-E.b <- E.r[rev(0:101), ]
-L.b <- L.r[rev(0:101), ]
-S.b <- S.r[rev(0:101), ]
+df.sp <- generate.rand.sp(100)
 
-df <- na.omit(data.frame(E = as.vector(E.b),
-                         L = as.vector(L.b),
-                         S = as.vector(S.b)))
-num.sp.select <- sample(sample(1:dim(df)[1], 100), round(NN*NN*Nlandscape*0.5), replace = TRUE)
+df.samp <- df.sp[sample(1:nrow(df.sp), round(NN*NN*Nlandscape*0.5), replace = TRUE), ]
+plot.sp(df.samp)
 
 ## fun to create random early succ and late succ competitive ability
-vec.landE[init.temp] <- df$E[num.sp.select]/100
-vec.landL[init.temp] <- df$L[num.sp.select]/100
-vec.land[init.temp] <- df$S[num.sp.select]/100
-
-par(mfrow=c(2,2))
-x   <- cbind(vec.landE[init.temp] ,vec.landL[init.temp])
-data <- as.data.frame(x)
-names(data) <- c("X","Y")
-colors  <- densCols(x)
-plot(x, col=colors, pch=20,cex=0.25,xlab="C.E",ylab="C.L")
-abline(a = 1, b = -1, col = 'red')
-x   <- cbind(1-vec.land[init.temp] ,vec.landL[init.temp])
-data <- as.data.frame(x)
-names(data) <- c("X","Y")
-colors  <- densCols(x)
-plot(x, col=colors, pch=20,cex=0.25,xlab="S",ylab="C.L")
-abline(a = 1, b = -1, col = 'red')
-x   <- cbind(vec.landE[init.temp] ,1-vec.land[init.temp])
-data <- as.data.frame(x)
-names(data) <- c("X","Y")
-colors  <- densCols(x)
-plot(x, col=colors, pch=20,cex=0.25,xlab="C.E",ylab="S")
-abline(a = 1, b = -1, col = 'red')
+mat.state.init[init.temp, "E"] <- df.samp$E
+mat.state.init[init.temp, "L"] <- df.samp$L
+mat.state.init[init.temp, "Succ"] <- 2
 
 
-vec.land.Succ[init.temp] <-  rep("E",round(NN*NN*Nlandscape*0.5))
-AlandscapeE <- matrix(vec.landE,nrow=NN,ncol=NN*Nlandscape)
-AlandscapeL <- matrix(vec.landL,nrow=NN,ncol=NN*Nlandscape)
-Alandscape.Succ <- matrix(vec.land.Succ,nrow=NN,ncol=NN*Nlandscape)
 
-Alandscape.LIST.init <-  list(AlandscapeE,AlandscapeL,Alandscape.Succ)
+## create a table of 8 neigborhood cells index with torus and reflection
+mat.neigh <- function.mat.neigcells(NN,Nlandscape)
 
-## create a table of 8 neigborhood cells
+Rprof("/tmp/succ.prof")
+res <- fun.run.sim.succ(A = 3,B = 30, mat.state = mat.state.init, mat.neigh, Ncells = nrow(mat.neigh), df.sp = df.sp, param.K = 5, param.dist = 0.1, param.succ = 0.25)
+Rprof(NULL)
+head(summaryRprof("/tmp/succ.prof")$by.self, 10)
 
-list.temp <- function.array.neigcells(NN,Nlandscape)
+system.time(
+    res <- fun.run.sim.succ(A = 4,B = 100, mat.state = mat.state.init, mat.neigh, Ncells = nrow(mat.neigh), df.sp = df.sp, param.K = 5, param.dist = 0.1, param.succ = 0.25)
+)
+hist(res[[5]][, 1])
+hist(res[[5]][, 2])
+plot(res[[5]][,1], res[[5]][,2])
 
-image(matrix(1:625 %in% mat.temp[100,],NN,NN*Nlandscape))
-mat.temp <- function.mat.neigcells(NN,Nlandscape)
-vec.e <- sample((0:100)/100, size = NN*NN*Nlandscape,replace = TRUE)
-neigh.val <- cbind(vec.e,
-                   matrix(vec.e[as.vector(mat.temp)], NN*NN*Nlandscape, 8))
-mat.prob <- t(apply(neigh.val, MARGIN = 1, function.compet, K = 5))
+sapply(res, function(x) table(x[,3]))
+table(res[[2]][, 3])
 
-sample.mat <-  function(x,y){
-    sample(x, size = 1, prob = y)
-}
-
-sampled.cell <- mapply(sample.mat, split(neigh.val, row(neigh.val)),
-                                   split(mat.prob, row(mat.prob)))
-
-array.i <- list.temp[[1]]
-array.j <- list.temp[[2]]
-rm(list.temp)
-
-matrix(1:prod(dim(Alandscape)),
 ##################################
 ##################################
 #### RUN A SIMULATION SUCCESSION TES  FOR A B time step
 ## K 100 P 1 Succ 0.25
 
-Rprof("/tmp/succ.prof")
-res.test <- fun.run.sim.Succ(A=1,B=200,
-                        Alandscape.LIST.init=Alandscape.LIST.init,
-                        fun.clim.morta=fun.clim.morta1,
-                        param.K=5,
-                        param.climate.stress=NA,
-                        param.dist=0.1,
-                        param.Succ=0.25,
-                        array.i = array.i,
-                        array.j = array.j)
-Rprof(NULL)
-head(summaryRprof("/tmp/succ.prof")$by.self, 10)
 
 fun.coex <- function(param.S){
-res <- fun.run.sim.Succ(A=1,B=800,
-                        Alandscape.LIST.init=Alandscape.LIST.init,
-                        fun.clim.morta=fun.clim.morta1,
-                        param.K=5,
-                        param.climate.stress=NA,
-                        param.dist=0.1,
-                        param.Succ=param.S,
-                        array.i = array.i,
-                        array.j = array.j)
-return(res[[1]][[2]])
+res <- fun.run.sim.succ(A = 1,B = 400, mat.state = mat.state.init, mat.neigh,
+                        Ncells = nrow(mat.neigh), df.sp = df.sp, param.K = 5,
+                        param.dist = 0.1, param.succ = 0.25)
+return(res[[2]])
 }
 
 res_list <- lapply(seq(0.1, 0.5, by = 0.05), fun.coex)
 image(sapply(res_list,function.table.levels), col = rev(heat.colors(50)))
 
-fun.image.landscape.Succ(res.test, 11)
 
-image(sapply(res.test[[1]],function.table.levels), col = rev(heat.colors(50)))
